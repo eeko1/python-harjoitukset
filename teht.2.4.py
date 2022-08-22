108b347b9dfdd61ef6ba1c08d5933c9cc3817efc
eeko1 <eemi.korhonen@metropolia.fi>
2022-08-22 14:59:38 +0300

    eka commit (tehtävä valmis)

---
+++ teht.2.4.py
@@ -0,0 +1,11 @@
+eka = int(input("Anna 1. kokonaisluku"))
+toka = int(input("Anna 2. kokonaisluku"))
+kolmas = int(input("Anna 3. kokonaisluku"))
+
+summa = eka + toka + kolmas
+tulo = eka * toka * kolmas
+ka = summa / 3
+
+print(summa)
+print(tulo)
+print(ka)
