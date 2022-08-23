d1402e70659d97b2cf9142d2d3562948457512c5
eeko1 <eemi.korhonen@metropolia.fi>
2022-08-23 10:38:17 +0300

    eka commit (tehtävä valmis)

--- teht.2.6.py
+++ teht.2.6.py
@@ -1,4 +1,17 @@
 import random
+#alla oleva koodi tulostaa kolmenumeroisen koodin,
+#jonka kukin numeromerkki on väliltä 0-9
+num1 = random.randint(0, 9)
+num2 = random.randint(0, 9)
+num3 = random.randint(0, 9)
 
-x = random.randint(0,9)
-print(x)
+print(num1,num2,num3)
+#alla oleva koodi tulostaa neljänumeroisen koodin,
+#jonka kukin numeromerkki on väliltä 1-6
+
+num4 = random.randint(1, 6)
+num5 = random.randint(1, 6)
+num6 = random.randint(1, 6)
+num7 = random.randint(1, 6)
+
+print(num4,num5,num6,num7)
