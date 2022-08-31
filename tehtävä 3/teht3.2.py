a6a5586e64d8f1d5d2fe0a2c9377d6fd25237512
eeko1 <eemi.korhonen@metropolia.fi>
2022-08-31 21:21:56 +0300

    tehtävä 3.2 valmis

---
+++ tehtävä 3/teht3.2.py
@@ -0,0 +1,18 @@
+
+hyttiluokka = input("Mikä on sinun hyttiluokka? (LUX,A,B,C)")
+
+if hyttiluokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+
+if hyttiluokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+
+if hyttiluokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+
+if hyttiluokka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+
+
+
+
