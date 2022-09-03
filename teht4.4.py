c95cda91254f91f48dea0337f58aaccdbfa31c8c
eeko1 <eemi.korhonen@metropolia.fi>
2022-09-03 18:57:43 +0300

    tehtävä 4.4 valmis

---
+++ teht4.4.py
@@ -0,0 +1,15 @@
+import random
+
+pelaajanluku = int(input("Anna luku 1-10 väliltä:"))
+luku = random.randint(1, 10)
+
+while pelaajanluku < luku:
+    print("Liian pieni arvaus")
+    pelaajanluku = int(input("Anna luku 1-10 väliltä:"))
+
+    if pelaajanluku > luku:
+        print("Liian suuri arvaus")
+        pelaajanluku = int(input("Anna luku 1-10 väliltä:"))
+
+    if pelaajanluku == luku:
+        print("Oikein")
