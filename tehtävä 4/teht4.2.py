5bc86c1c2f90750b93304cbabc0b8e247d85a5fe
eeko1 <eemi.korhonen@metropolia.fi>
2022-09-03 17:38:28 +0300

    tehtävä 4.2 valmis

---
+++ tehtävä 4/teht4.2.py
@@ -0,0 +1,6 @@
+#ohjelma muuntaa tuumat senttimetreiksi.
+tuuma = float(input("Anna tuuman luku:"))
+
+while tuuma >= 0:
+    print (f"Luku on {tuuma * 2.51} cm")
+    tuuma = int(input("Anna tuuman luku:"))
