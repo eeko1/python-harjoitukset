29c4a5af74994c10d087c8464d51bc05fc606336
eeko1 <eemi.korhonen@metropolia.fi>
2022-09-12 15:23:49 +0300

    tehtävä 8.2 valmis

---
+++ Tehtävä 8/teht8.2.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+# yhdeyden avaus
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password= 'RootWord1Salasana1',
+         autocommit=True
+         )
+#   määritellään kysely
+maakoodi = input("Kirjoita maakoodi")
+sql = "SELECT type, COUNT(*) FROM airport WHERE iso_country = '" + maakoodi + "' GROUP BY TYPE"
+
+# suoritetaan kysely
+kursori = yhteys.cursor()
+kursori.execute(sql)
+
+tulos = kursori.fetchall()
+for rivi in tulos:
+    print(f"{rivi[0]}, {rivi[1]}")
