eb9cc26d8f1582fcee4977cfc57f80497be947d5
eeko1 <eemi.korhonen@metropolia.fi>
2022-09-12 12:43:54 +0300

    tehtävä 7.1 valmis

---
+++ tehtävä 7/teht7.1.py
@@ -0,0 +1,13 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron, jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan
+# (kevät, kesä, syksy, talvi). Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
+# Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
+
+
+vuodenAjat = ("talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
+
+kuukausi = int(input("Kirjoita kuukausi numeroina 0-11: (0 = tammikuu 11 = joulukuu)"))
+
+print(f"{kuukausi} on {vuodenAjat[kuukausi]} kuukausi")
+
+
+
