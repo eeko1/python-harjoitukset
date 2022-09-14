510d256cd019ea56590f4520a5fc9d65d544ab36
eeko1 <eemi.korhonen@metropolia.fi>
2022-09-14 13:32:34 +0300

    tehtävä 7.2 valmis (korjattu huomattu virhe)

--- tehtävä 7/teht7.2.py
+++ tehtävä 7/teht7.2.py
@@ -1,7 +1,7 @@
 #Kirjoita ohjelma, joka kysyy käyttäjältä nimiä siihen saakka, kunnes käyttäjä syöttää tyhjän merkkijonon.
 # Kunkin nimen syöttämisen jälkeen ohjelma tulostaa joko tekstin Uusi nimi tai Aiemmin syötetty nimi sen mukaan, syötettiinkö nimi ensimmäistä kertaa.
 # Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa järjestyksessä. Käytä joukkotietorakennetta nimien tallentamiseen.
-nimi = {"Milla"}
+nimi = set()
 
 while True:
     userInput = input("Kirjoita nimi:")
