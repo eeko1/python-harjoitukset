e09c92c50768ec8e061415c1e21b2f65dcd09dce
eeko1 <eemi.korhonen@metropolia.fi>
2022-11-01 15:53:36 +0200

    10.2 tehty. Palautus tuli myöhässä.

--- Tehtävä 10/teht10.2.py
+++ Tehtävä 10/teht10.2.py
@@ -7,32 +7,33 @@
 
 class Elevator:
 
-    def __init__(self,low, top, current):
+    def __init__(self, low, top, current):
         self.low = low
         self.top = top
         self.current = current
 
     def elevator_movement(self, floor):
         if self.current < floor:
-            Elevator.move_up(i)
+            Elevator.move_up(h)
         elif self.current > floor:
-            Elevator.move_down(i)
+            Elevator.move_down(h)
         elif self.current == floor:
-            print("You are already there")
+            print("You are already at that floor")
 
     def move_up(self): #kerros_ylös
         while self.current != floor:
+            Elevator.printing(self)
             self.current += 1
             if self.current == floor:
-                Elevator.printing(i)
+                Elevator.printing(h)
                 print(f"Currently at: Floor{self.current}")
 
     def move_down(self): #kerros_alas
         while self.current != floor:
-            Elevator.printing(i)
+            Elevator.printing(self)
             self.current -= 1
             if self.current == floor:
-                Elevator.printing(i)
+                Elevator.printing(h)
                 print(f"Currently at: Floor{self.current}")
 
     def printing(self):
@@ -47,18 +48,17 @@ class House:
         self.number = number
         self.current = current
         self.Elevators = []
-        for i in range(1, 4):
-           self.Elevators.append(Elevator(1, 50, 1))
+        for i in range(number):
+            self.Elevators.append(Elevator(1, 4, 1))
 
-    def aja(self, number, kohde):
+    def drive(self, number, what_floor):
         elevator = self.Elevators[number - 1]
-        print(f"Ajetaan hissiä {number}")
+        print(f"Elevator {number} moving")
 
-
-i = Elevator(1, 50, 1)    # i = h
 floor = 1
-Haus = House(1, 50, 3, 1)
-while floor < 51:
-    Elevator_number = int(input("Valitse hissi"))
-    what_floor = int(input("Mihin kerrokseen haluat?"))
-    Haus.aja(Elevator_number, what_floor)
+h = Elevator(1, 4, 1)
+Haus = House(1, 5, 3, 1)
+while floor < 6:
+    Elevator_number = int(input("Choose an elevator:"))
+    what_floor = int(input("What floor would you like to go?"))
+    Haus.drive(Elevator_number, what_floor)
